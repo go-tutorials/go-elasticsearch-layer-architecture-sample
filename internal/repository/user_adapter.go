fbf28ca0588c1ad539f8767824e34063fea6543d
Duc Nguyen <minhduc140583@gmail.com>
2023-08-06 18:41:04 +0700

    Apply latest version

--- internal/repository/user_adapter.go
+++ internal/repository/user_adapter.go
@@ -5,21 +5,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
-	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
 	"reflect"
 	"strings"
 
 	"go-service/internal/model"
 )
 
-type ElasticSearchUserAdapter struct {
-	elastic *elasticsearch.Client
+type UserAdapter struct {
+	client *elasticsearch.Client
 }
 
-func NewUserRepository(ela *elasticsearch.Client) *ElasticSearchUserAdapter {
-	return &ElasticSearchUserAdapter{elastic: ela}
+func NewUserRepository(client *elasticsearch.Client) *UserAdapter {
+	return &UserAdapter{client: client}
 }
 
 func convertDocToJson(doc interface{}) string {
@@ -32,48 +32,31 @@ func convertDocToJson(doc interface{}) string {
 	return string(jsonString)
 }
 
-func SetField(obj interface{}, name string, value interface{}) error {
-	structValue := reflect.ValueOf(obj).Elem()
-	structFieldValue := structValue.FieldByName(name)
-
-	if !structFieldValue.IsValid() {
-		return fmt.Errorf("no such field: %s in obj", name)
-	}
-
-	if !structFieldValue.CanSet() {
-		return fmt.Errorf("cannot set %s field value", name)
-	}
-
-	val := reflect.ValueOf(value)
-	structFieldValue.Set(val)
-	return nil
-}
-
-func (e *ElasticSearchUserAdapter) All(ctx context.Context) (*[]model.User, error) {
-	var listUser []model.User
+func (e *UserAdapter) All(ctx context.Context) ([]model.User, error) {
+	var users []model.User
 	var mapResponse map[string]interface{}
 	var buf bytes.Buffer
 
 	query := `{
   "query": {
     "match_all": {}
-  },
-  "size": 1
+  }
 }`
 
 	var queryString = strings.NewReader(query)
 
-	err := json.NewEncoder(&buf).Encode(queryString)
+	err := json.NewEncoder(&buf).Encode(&queryString)
 	if err != nil {
 		fmt.Print("error during encoding the query: ", err.Error())
+		return users, err
 	}
 
-	result, err := e.elastic.Search(
-		e.elastic.Search.WithContext(ctx),
-		e.elastic.Search.WithIndex("users"),
-		e.elastic.Search.WithBody(queryString),
-		e.elastic.Search.WithTrackTotalHits(true),
-		e.elastic.Search.WithPretty(),
+	result, err := e.client.Search(
+		e.client.Search.WithContext(ctx),
+		e.client.Search.WithIndex("users"),
+		e.client.Search.WithBody(queryString),
+		e.client.Search.WithTrackTotalHits(true),
+		e.client.Search.WithPretty(),
 	)
 	defer result.Body.Close()
 
@@ -81,29 +64,33 @@ func (e *ElasticSearchUserAdapter) All(ctx context.Context) (*[]model.User, erro
 	fmt.Println("This is map response: ", mapResponse)
 	if err != nil {
 		fmt.Println("Error parsing the result to User type:", err.Error())
+		return users, err
 	}
 
 	var u = &model.User{}
-	//var decodedUser model.User{}
 	for _, hit := range mapResponse["hits"].(map[string]interface{})["hits"].([]interface{}) {
 		user := hit.(map[string]interface{})
 
 		source := user["_source"]
-		//userId := user["_id"]
-		//u = DecodeMapToStruct(user)
+		u.Id = user["_id"].(string)
 		fmt.Println("This is the source:")
-
 		fmt.Println(source)
-		bytes, _ := json.Marshal(source)
-		_ = json.Unmarshal(bytes, u)
-		listUser = append(listUser, *u)
+
+		bytes, err := json.Marshal(source)
+		if err != nil {
+			return users, err
+		}
+		err = json.Unmarshal(bytes, u)
+		if err != nil {
+			return users, err
+		}
+		users = append(users, *u)
 
 	}
-	return &listUser, nil
+	return users, nil
 }
 
-func (e *ElasticSearchUserAdapter) Load(ctx context.Context, id string) (*model.User, error) {
-	var listUser []model.User
+func (e *UserAdapter) Load(ctx context.Context, id string) (*model.User, error) {
 	var mapResponse map[string]interface{}
 	var buf bytes.Buffer
 
@@ -117,46 +104,52 @@ func (e *ElasticSearchUserAdapter) Load(ctx context.Context, id string) (*model.
 	query = strings.Replace(query, "{0}", id, 1)
 	var queryString = strings.NewReader(query)
 
-	err := json.NewEncoder(&buf).Encode(queryString)
+	err := json.NewEncoder(&buf).Encode(&queryString)
 	if err != nil {
 		fmt.Print("Error during encoding the query : ", err.Error())
+		return nil, err
 	}
 
-	result, err := e.elastic.Search(
-		e.elastic.Search.WithContext(ctx),
-		e.elastic.Search.WithIndex("users"),
-		e.elastic.Search.WithBody(queryString),
-		e.elastic.Search.WithTrackTotalHits(true),
-		e.elastic.Search.WithPretty(),
+	result, err := e.client.Search(
+		e.client.Search.WithContext(ctx),
+		e.client.Search.WithIndex("users"),
+		e.client.Search.WithBody(queryString),
+		e.client.Search.WithTrackTotalHits(true),
+		e.client.Search.WithPretty(),
 	)
 	defer result.Body.Close()
 
 	err = json.NewDecoder(result.Body).Decode(&mapResponse)
+	if err != nil {
+		return nil, err
+	}
 	fmt.Println("This is map response: ", mapResponse)
 	if err != nil {
 		fmt.Println("Error parsing the result to User type:", err.Error())
 	}
 
 	var u = &model.User{}
-	//var decodedUser model.User{}
 	for _, hit := range mapResponse["hits"].(map[string]interface{})["hits"].([]interface{}) {
 		user := hit.(map[string]interface{})
 
 		source := user["_source"]
-		//userId := user["_id"]
-		//u = DecodeMapToStruct(user)
 		fmt.Println("This is the source:")
-
 		fmt.Println(source)
-		bytes, _ := json.Marshal(source)
-		_ = json.Unmarshal(bytes, u)
-		listUser = append(listUser, *u)
 
+		bytes, err:= json.Marshal(source)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(bytes, u)
+		if err != nil {
+			return nil, err
+		}
+		return u, nil
 	}
-	return u, nil
+	return nil, nil
 }
 
-func (e *ElasticSearchUserAdapter) Create(ctx context.Context, user *model.User) (int64, error) {
+func (e *UserAdapter) Create(ctx context.Context, user *model.User) (int64, error) {
 	if user == nil {
 		fmt.Print("Can not add null user")
 		return 0, nil
@@ -169,11 +162,10 @@ func (e *ElasticSearchUserAdapter) Create(ctx context.Context, user *model.User)
 		Body:       strings.NewReader(userJsonString),
 		Refresh:    "true",
 	}
-	response, err := request.Do(ctx, e.elastic)
+	response, err := request.Do(ctx, e.client)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return 0, err
 	}
 
 	defer response.Body.Close()
@@ -183,8 +175,7 @@ func (e *ElasticSearchUserAdapter) Create(ctx context.Context, user *model.User)
 	err = json.NewDecoder(response.Body).Decode(&result)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	fmt.Println("IndexRequest to insert Status: ", response.Status())
@@ -194,7 +185,7 @@ func (e *ElasticSearchUserAdapter) Create(ctx context.Context, user *model.User)
 	return 1, nil
 }
 
-func (e *ElasticSearchUserAdapter) Update(ctx context.Context, user *model.User) (int64, error) {
+func (e *UserAdapter) Update(ctx context.Context, user *model.User) (int64, error) {
 	query := map[string]interface{}{
 		"doc": user,
 	}
@@ -204,11 +195,10 @@ func (e *ElasticSearchUserAdapter) Update(ctx context.Context, user *model.User)
 		Body:       esutil.NewJSONReader(query),
 		Refresh:    "true",
 	}
-	response, err := request.Do(ctx, e.elastic)
+	response, err := request.Do(ctx, e.client)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	defer response.Body.Close()
@@ -218,31 +208,29 @@ func (e *ElasticSearchUserAdapter) Update(ctx context.Context, user *model.User)
 	err = json.NewDecoder(response.Body).Decode(&result)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	fmt.Println("IndexRequest to update Status: ", response.Status())
 	fmt.Println("Result: ", result)
 
-	fmt.Print("the user %v has been updated successfully", user.Username)
+	fmt.Printf("the user %v has been updated successfully", user.Username)
 	return 1, nil
 }
 
-func (e *ElasticSearchUserAdapter) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
-	var userid = reflect.ValueOf(user["id"])
+func (e *UserAdapter) Patch(ctx context.Context, user map[string]interface{}) (int64, error) {
+	var userId = reflect.ValueOf(user["id"])
 	delete(user, "id")
 	request := esapi.UpdateRequest{
 		Index:      "users",
-		DocumentID: userid.String(),
+		DocumentID: userId.String(),
 		Body:       esutil.NewJSONReader(map[string]interface{}{"doc": user}),
 		Refresh:    "true",
 	}
-	response, err := request.Do(ctx, e.elastic)
+	response, err := request.Do(ctx, e.client)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	defer response.Body.Close()
@@ -252,27 +240,25 @@ func (e *ElasticSearchUserAdapter) Patch(ctx context.Context, user map[string]in
 	err = json.NewDecoder(response.Body).Decode(&result)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	fmt.Println("IndexRequest to update Status: ", response.Status())
 	fmt.Println("Result: ", result["result"])
 
-	fmt.Print("the user %v has been updated successfully.", userid.String())
+	fmt.Printf("the user %v has been updated successfully.", userId.String())
 	return 1, nil
 }
 
-func (e *ElasticSearchUserAdapter) Delete(ctx context.Context, id string) (int64, error) {
+func (e *UserAdapter) Delete(ctx context.Context, id string) (int64, error) {
 	request := esapi.DeleteRequest{
 		Index:      "users",
 		DocumentID: id,
 	}
-	response, err := request.Do(ctx, e.elastic)
+	response, err := request.Do(ctx, e.client)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	defer response.Body.Close()
@@ -282,13 +268,12 @@ func (e *ElasticSearchUserAdapter) Delete(ctx context.Context, id string) (int64
 	err = json.NewDecoder(response.Body).Decode(&result)
 
 	if err != nil {
-		panic(err)
-		return 0, nil
+		return -1, err
 	}
 
 	fmt.Println("IndexRequest to update Status: ", response.Status())
 	fmt.Println("Result: ", result["result"])
 
-	fmt.Print("delete user: %s successfully", id)
+	fmt.Printf("delete user: %s successfully", id)
 	return 1, nil
 }
